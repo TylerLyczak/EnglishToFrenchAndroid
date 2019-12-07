26fa1328971a3a29286f0aa6b27b17343248505d
TylerLyczak <tyler.m.lyczak@gmail.com>
2019-12-06 18:32:15 -0600

    added comments

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -9,9 +9,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Called from any button press
     public void playAudio (View view)   {
+        // Converts the view into a button
         Button button = (Button)view;
+        // Since each button has a tag of the audio, this will create a media player from the button's tag
         MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(button.getTag().toString(), "raw", getPackageName()));
+        // Plays the audio
         mediaPlayer.start();
     }
 
